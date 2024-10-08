ab48df07135114c4fce6527533093e601075eae2
amine romdhani <ramdhaniahmedamine@gmail.com>
2024-10-08 13:02:20 +0100

    Delete test.php

--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-$pythonScriptPath = "C:\\xampp\\htdocs\\Stage\\python_script"; // Replace with your actual path
-$command = 'cd ' . $pythonScriptPath . ' && python main.py'; // Navigate to the directory and execute the script
-$output = shell_exec($command);
-
-echo "<pre>$output</pre>";
-?>
